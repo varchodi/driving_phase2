5ed301b4421d151f5ffc6828eacbce2496b1ac57
varchodi <rauschenrobert@gmail.com>
2024-04-30 17:00:25 +0300

    Pseudo-3d: trees : make them look like trees

--- src/items/tree.ts
+++ src/items/tree.ts
@@ -1,5 +1,6 @@
-import {scale, add, substract,lerp2D, lerp } from "../math/utils";
+import {scale, add, substract,lerp2D, lerp, translate } from "../math/utils";
 import { Point } from "../primitives/point";
+import { Polygon } from "../primitives/polygon";
 import { Segment } from "../primitives/segment";
 
 export class Tree{
@@ -7,6 +8,18 @@ export class Tree{
         this.center = center;
         this.size = size; // size of base 
         this.hightCoef=hightCoef // tree hight controller
+    }
+    private generateLevel(point: Point, size: number):Polygon {
+        const points = [];
+        const rad = size / 2;
+
+        for (let a = 0; a < Math.PI * 2; a += Math.PI / 16){
+            const kindOfRandom = Math.cos(((a + this.center.x) * size) % 17)**2;
+            const noisyRadius = rad * lerp(0.5,1,kindOfRandom);
+            points.push(translate(point, a, noisyRadius));
+        }
+
+        return new Polygon(points);
     }
 
     draw(ctx: CanvasRenderingContext2D, viewPoint:Point) {
@@ -21,8 +34,9 @@ export class Tree{
             const point = lerp2D(this.center, top, t);
             const color = `rgb(30,${lerp(50, 200, t)},70)`;// level colors (shading effect)
             const size = lerp(this.size, 40, t);
-            point.draw(ctx, { size,color });
             
+            const poly = this.generateLevel(point, size);
+            poly.draw(ctx,{fill:color,stroke:"rgba(0,0,0,0)"})
         }
         
     }
