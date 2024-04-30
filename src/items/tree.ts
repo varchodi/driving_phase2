9b626fd1c866b7d65552c906f887480da803d271
varchodi <rauschenrobert@gmail.com>
2024-04-30 16:31:09 +0300

    Pseudo-3d: add a tree hight controller

--- src/items/tree.ts
+++ src/items/tree.ts
@@ -1,18 +1,19 @@
-import { add, substract } from "../math/utils";
+import {scale, add, substract } from "../math/utils";
 import { Point } from "../primitives/point";
 import { Segment } from "../primitives/segment";
 
 export class Tree{
-    constructor(public center: Point, public size: number) {
+    constructor(public center: Point, public size: number,private hightCoef:number=0.3) {
         this.center = center;
         this.size = size; // size of base 
+        this.hightCoef=hightCoef // tree hight controller
     }
 
     draw(ctx: CanvasRenderingContext2D, viewPoint:Point) {
         
         const diff=substract(this.center,viewPoint)
         this.center.draw(ctx, { size: this.size, color: "green" })
-        const top = add(this.center, diff);
+        const top = add(this.center, scale(diff,this.hightCoef));
         new Segment(this.center, top).draw(ctx);
     }
 }
