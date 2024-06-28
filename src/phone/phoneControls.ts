9131f7091e1e4ac78cd0f818428c156af3c95571
varchodi <rauschenrobert@gmail.com>
2024-06-28 15:23:43 +0300

    titling like matre d eau

--- src/phone/phoneControls.ts
+++ src/phone/phoneControls.ts
@@ -1,16 +1,29 @@
 export class PhoneControls{
     public tilt: number;
+    public canvasAngle: number;
     constructor(public canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.tilt = 0;
+        this.canvasAngle = 0;
         this.addAdventListeners();
     }
 
     private addAdventListeners() {
-        window.addEventListener('deviceorientation', (e: DeviceOrientationEvent) => {
-            this.tilt = e.beta! * Math.PI / 180;
-            const canvasAngle = -this.tilt;
-            this.canvas.style.transform =`translate(-50%,-50%) rotate(${canvasAngle}rad)`
+        // ---- Ref - Basic titling -----
+        // window.addEventListener('deviceorientation', (e: DeviceOrientationEvent) => {
+        //     this.tilt = e.beta! * Math.PI / 180;
+        //     const canvasAngle = -this.tilt;
+        //     this.canvas.style.transform =`translate(-50%,-50%) rotate(${canvasAngle}rad)`
+        // })
+
+        window.addEventListener('devicemotion', (e: DeviceMotionEvent) => {
+            this.tilt = Math.atan2(
+                e.accelerationIncludingGravity?.y!,
+                e.accelerationIncludingGravity?.x!
+            )
+            const newCanvasAngle = -this.tilt;
+            this.canvasAngle = this.canvasAngle * .6  + newCanvasAngle*.4;
+            this.canvas.style.transform =`translate(-50%,-50%) rotate(${this.canvasAngle}rad)`
         })
 
     }
