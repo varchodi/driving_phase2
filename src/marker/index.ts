d60052943621caf515bfc38c93383da0c9df9e33
varchodi <rauschenrobert@gmail.com>
2024-07-01 15:00:03 +0300

    draw images(video) on canvas

--- src/marker/index.ts
+++ src/marker/index.ts
@@ -1,4 +1,16 @@
-const myVideo = document.getElementById("myVideo") as HTMLVideoElement;
+// let myVideo = document.getElementById("myVideo") as HTMLVideoElement;
+const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+
+const myVideo = document.createElement('video')!;
+const ctx = myCanvas.getContext('2d')!;
+
+const loop = () => {
+    
+    //draw video on canvas
+    ctx.drawImage(myVideo, 0, 0, myCanvas.width, myCanvas.height);
+    requestAnimationFrame(loop);
+    
+}
 
 // get camera access
 navigator.mediaDevices.getUserMedia({
@@ -6,6 +18,15 @@ navigator.mediaDevices.getUserMedia({
 }).then((rawData:MediaStream) => {
     myVideo.srcObject = rawData; // display camera data  
     myVideo.play() //play video
+
+    myVideo.onloadeddata = () => {
+        myCanvas.width = myVideo.videoWidth // set to same width ass video stream
+        myCanvas.height = myVideo.videoHeight
+        
+        //draw video on canvas
+        // ctx.drawImage(myVideo,0,0,myCanvas.width,myCanvas.height) // draw only one frame ( 1rst)
+        loop() //draw video on canvas
+    }
 }).catch((error) => {
     alert(error)
 })
