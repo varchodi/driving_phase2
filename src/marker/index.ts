352a5aba66aa9ee393186f58630c3c7d312f6dd5
varchodi <rauschenrobert@gmail.com>
2024-07-01 14:40:59 +0300

    camera video player

--- src/marker/index.ts
+++ src/marker/index.ts
@@ -1,1 +1,11 @@
 const myVideo = document.getElementById("myVideo") as HTMLVideoElement;
+
+// get camera access
+navigator.mediaDevices.getUserMedia({
+    video:true
+}).then((rawData:MediaStream) => {
+    myVideo.srcObject = rawData; // display camera data  
+    myVideo.play() //play video
+}).catch((error) => {
+    alert(error)
+})
